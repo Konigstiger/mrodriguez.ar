d2f67cac85956423f15113744f1c8dc9e42b6643
Konigstiger <merodriguez81@gmail.com>
2021-02-09 03:54:51 -0300

    Updates

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CracoAlias = require("craco-alias");
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, 'src'),
+    }
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.paths.json",
+      },
+    },
+  ],
+};
