24866a66530fcba51fdda6f57af1d2dac7d32423
Konigstiger <merodriguez81@gmail.com>
2021-03-17 14:39:14 -0300

    Updated repo to React (js)

---
+++ home/src/App.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import './App.css';
+
+// TODO: Add these components
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <Home />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
