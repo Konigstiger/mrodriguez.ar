ad77e006ca9b47e0129127499c07830a86a5ca13
Konigstiger <merodriguez81@gmail.com>
2022-02-28 09:39:45 -0300

    Small color changes

--- home/src/components/Home.tsx
+++ home/src/components/Home.tsx
@@ -1,28 +1,22 @@
 import "../index.css";
 
 import BioCard from "./BioCard";
-import StackedCard from "./StackedCard";
-import HorizontalCard from "./HorizontalCard";
 import StickyFooter from "./StickyFooter";
 
 import Articles from "./Articles";
-import Timeline from "./Timeline";
 
-import Banner from "./Banner";
-
-import illustration from "../images/csharp-02.png";
-
-function Home() {
+export default function Home() {
   return (
     <div>
-      <BioCard />
-
-      <Articles />
+      {/* <div className="bg-gray-700"> */}
+        <BioCard />
+      {/* </div> */}
+      <div className="bg-gray-600">
+        <Articles />
+      </div>
 
       <StickyFooter />
       {/* <Footer /> */}
     </div>
   );
 }
-
-export default Home;
