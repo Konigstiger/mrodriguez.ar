8bd926e52cff250514ad85608073b1e5a0113141
Konigstiger <merodriguez81@gmail.com>
2022-05-01 22:32:08 -0300

    Fix for mobile small

--- home/src/components/NavTitleHeader.tsx
+++ home/src/components/NavTitleHeader.tsx
@@ -3,7 +3,7 @@ import "../index.css";
 export default function NavTitleHeader({ title, subtitle }) {
   return (
     <div>
-      <div className="bg-secondary text-text-on-secondary font-bold rounded-t-lg px-4 py-0 font-poiretOne text-2xl">
+      <div className="bg-secondary text-text-on-secondary font-bold rounded-t-lg px-4 py-0 font-poiretOne text-xl">
         {title}
       </div>
       <div className="bg-secondary-dark text-text-on-secondary rounded-b-lg font-serif text-sm px-4">
