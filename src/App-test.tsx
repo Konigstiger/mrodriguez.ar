bbcf6785665383a05119f4ed2a6b2fe33e6d409d
Mariano Rodriguez <merodriguez81@gmail.com>
2023-08-16 14:41:39 -0300

    Upgrades in dependencies
    They seem to not break anything.
    I need to pay some tech debt with the repo and unused components soon.

--- src/App-test.tsx
+++ src/App-test.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
--- vite.config.tsx
+++ vite.config.tsx
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
 })
